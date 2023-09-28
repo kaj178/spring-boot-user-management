a646c53796b4b06d8f7523252e84920950a08abc
Anh Khoa <anhkhoa5525@gmail.com>
2023-09-28 21:03:51 +0700

    Created and updated entities

--- src/main/java/com/kaj/rest/api/model/entities/User.java
+++ src/main/java/com/kaj/rest/api/model/entities/User.java
@@ -3,20 +3,32 @@ package com.kaj.rest.api.model.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "user_info")
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String user_id;
+    @Column(name = "user_id")
+    private String userID;
 
-    @Column(name = "user_name")
-    private String name;
+    @Column(name = "user_last_name")
+    private String userLastName;
+
+    @Column(name = "user_first_name")
+    private String userFirstName;
+
+    @Column(name = "user_birthday")
+    @Temporal(TemporalType.DATE)
+    private LocalDate userBirthDay;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_phone")
+    private String userPhone;
 
     @Column(name = "user_gender")
-    private String gender;
-
-    @Column(name = "user_status")
-    private String status;
+    private String userGender;
 }
